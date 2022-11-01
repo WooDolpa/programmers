4a821877c7f47ccd1c9dec4a36256a98a883f17b
jwlee <dannis1124@naver.com>
2022-11-01 13:57:18 +0900

    coding test study

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * packageName :
+ * className : ${NAME}
+ * user : ${USER}
+ * date : ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
