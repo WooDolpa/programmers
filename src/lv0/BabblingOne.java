c59d43df5d13818610eb319ac61022eff486327f
jwlee <dannis1124@naver.com>
2022-11-11 22:59:40 +0900

    프로그래머스 LV0 옹알이(1) 코드 수정

--- src/lv0/BabblingOne.java
+++ src/lv0/BabblingOne.java
@@ -9,16 +9,29 @@ import java.util.stream.IntStream;
  * date : 2022/11/09
  */
 public class BabblingOne {
-
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println("result : "+ solution.solution(new String[]{"aya","yee","u","maa","wyeoo"}));
+//        System.out.println("result : "+ solution.solution(new String[]{"aya","yee","u","maa","wyeoo"}));
+//        System.out.println("result : "+ solution.solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"}));
 
-//        String test = "wyeoo";
-//        String[] splitStr = test.split("ye");
-//        for (String str : splitStr) {
-//            System.out.println("..... : "+ str.trim() + "|" + str.trim().length());
+
+//        String test = "yemawoo";
+//        String[] keywords = {"ma","ye","woo"};
+//
+//        for (String keyword : keywords) {
+//            if(test.contains(keyword)) {
+//
+//                int stringIdx = test.indexOf(keyword);
+//                String newStr = test.substring(keyword.length());
+//                if(stringIdx > 0) {
+//                    newStr = test.substring(0, stringIdx);
+//                    newStr += test.substring(stringIdx+keyword.length());
+//                }
+//                test = newStr;
+//            }
 //        }
+        String[] samples = "wyeoo".split("ye");
+        System.out.println(samples[0] + samples[1]);
 
     }
 
@@ -48,38 +61,33 @@ public class BabblingOne {
         private boolean isBabbling(String babble) {
 
             String[] keywords = new String[]{"aya","ye","woo","ma"};
-            String test = babble;
+            String target = babble;
             for (String keyword : keywords) {
-                test = splitKeyword(test, keyword);
-                if(test.length() == 0) break;
+                target = keywordMatch(target, keyword);
+                if(target.length() == 0) return true;
             }
 
-            if(test.length() == 0) return true;
             return false;
         }
 
-        private String splitKeyword(String text, String keyword) {
 
-            String result = "";
+        private String keywordMatch(String target, String keyword) {
 
-            int count = 0;
-            String[] splitStrings = text.split(keyword);
+            if(target.contains(keyword)) {
 
-            if(splitStrings.length == 0) return "";
-
-            for (String splitString : splitStrings) {
-                if(splitString.length() > 1) {
-                    count++;
-                    result = splitString;
+                int keywordIndex = target.indexOf(keyword);
+                String newStr = target.substring(keyword.length());
+                if(keywordIndex > 0) {
+                    newStr = target.substring(0, keywordIndex);
+                    newStr += target.substring(keywordIndex + keyword.length());
                 }
+
+                return newStr;
             }
 
-            if(count == 1) {
-                return  result;
-            }
-
-            return text;
+            return target;
         }
+
     }
 
 }
