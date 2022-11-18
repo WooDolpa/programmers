c081f95b5db6a2594918b7d2042a11029b414dde
jwlee <dannis1124@naver.com>
2022-11-18 14:50:11 +0900

    코딩테스트 - Lv0 -> 나이출력, 각도기 문제 풀이

---
+++ src/lv0/AgeTest.java
@@ -0,0 +1,30 @@
+package lv0;
+
+/**
+ * packageName : lv0
+ * className : AgeTest
+ * user : jwlee
+ * date : 2022/11/17
+ */
+public class AgeTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 나이 출력
+     *
+     */
+    public static class Solution {
+        public int solution(int age) {
+            boolean isValidated =  (0 < age && age <= 120);
+            int fixYear = 2022;
+            int answer = 0;
+            if(isValidated) {
+                return (fixYear - age) + 1;
+            }
+            return answer;
+        }
+    }
+}
---
+++ src/lv0/ProtractorTest.java
@@ -0,0 +1,41 @@
+package lv0;
+
+/**
+ * packageName : lv0
+ * className : ProtractorTest
+ * user : jwlee
+ * date : 2022/11/17
+ */
+public class ProtractorTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 각도기 문제
+     *
+     */
+    public static class Solution {
+
+        public int solution(int angle) {
+            boolean isValidated = (0 < angle && angle <= 180);
+            int answer = 0;
+            if(isValidated) {
+                return getAngleType(angle);
+            }
+            return answer;
+        }
+
+        private int getAngleType (int angle) {
+            if(0 < angle && angle < 90) {
+                return 1;
+            }else if(angle == 90) {
+                return 2;
+            }else if (90 < angle && angle < 180) {
+                return 3;
+            }
+            return 4;
+        }
+    }
+}
