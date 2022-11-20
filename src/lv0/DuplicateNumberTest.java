1d2fb21ebd54c32bae2ce4347624dd6df21dfe97
jwlee <dannis1124@naver.com>
2022-11-20 15:31:37 +0900

    코딩테스트 - Lv0 -> 키큰사람 구하기, 중복된 숫자 구하기 풀이

---
+++ src/lv0/DuplicateNumberTest.java
@@ -0,0 +1,33 @@
+package lv0;
+
+import java.util.Arrays;
+
+/**
+ * packageName : lv0
+ * className : DuplicateNumberTest
+ * user : jwlee
+ * date : 2022/11/20
+ */
+public class DuplicateNumberTest {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[]{1, 1, 2, 3, 4, 5}, 1));
+        System.out.println(solution.solution(new int[]{0, 2, 3, 4}, 1));
+    }
+
+    /**
+     * 중복된 숫자 개수
+     *
+     */
+    public static class Solution {
+        public int solution(int[] array, int n) {
+            boolean isValid = (1 <= array.length && array.length <= 100) && (0 <= n && n <= 1000);
+            int answer = 0;
+            if(isValid) {
+                return (int) Arrays.stream(array).filter(i -> {return (i == n);}).count();
+            }
+            return answer;
+        }
+    }
+}
---
+++ src/lv0/TallTest.java
@@ -0,0 +1,42 @@
+package lv0;
+
+import java.util.Arrays;
+
+/**
+ * packageName : lv0
+ * className : TallTest
+ * user : jwlee
+ * date : 2022/11/20
+ */
+public class TallTest {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[]{149, 180, 192, 170}, 167));
+        System.out.println(solution.solution(new int[]{180, 120, 140}, 190));
+    }
+
+    /**
+     * 키 큰 사람 구하기
+     *
+     */
+    public static class Solution {
+        public int solution(int[] array, int height) {
+
+            boolean isValid = (1 <= array.length && array.length <= 100) && (1 <= height && height <= 200);
+            int answer = 0;
+            if(isValid) {
+                for (int i=0; i<array.length; i++) {
+                    int arrayHeight = array[i];
+                    if(1 <= arrayHeight && arrayHeight <= 200) {
+                        if(arrayHeight > height) {
+                            answer++;
+                        }
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+    // return (int) Arrays.stream(array).filter(value -> value > height).count();
+}
