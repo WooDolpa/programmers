564386e600f2e77fa522f174f2a6abd0f6b4dbe6
jwlee <dannis1124@naver.com>
2022-11-26 23:16:41 +0900

    코딩테스트 Lv0 -> 삼각형 완성조건 (1) 푸는중

---
+++ src/lv0/day07/TriangleCPOneTest.java
@@ -0,0 +1,29 @@
+package lv0.day07;
+
+/**
+ * packageName : lv0.day07
+ * className : TriangleCPOneTest
+ * user : jwlee
+ * date : 2022/11/24
+ */
+public class TriangleCPOneTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 삼각형 완성조건 (1)
+     *
+     */
+    public static class Solution {
+        public int solution(int[] sides) {
+            boolean isValidLen = (sides.length == 3);
+            int answer = 0;
+            if(isValidLen){
+
+            }
+            return answer;
+        }
+    }
+}
