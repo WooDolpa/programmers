caa38515f46aa018905dd2623af88ec2c2cd890a
jwlee <dannis1124@naver.com>
2022-12-20 21:24:46 +0900

    코딩테스트 Lv0 -> 직사각형 넓이 풀이

---
+++ src/lv0/day22/RectangleTest.java
@@ -0,0 +1,25 @@
+package lv0.day22;
+
+/**
+ * packageName : lv0.day22
+ * className : RectangleTest
+ * user : jwlee
+ * date : 2022/12/19
+ */
+public class RectangleTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Solution {
+        public int solution(int[][] dots) {
+            int answer = 0;
+            boolean isDotsLen = dots.length == 4;
+            if(isDotsLen) {
+
+            }
+            return answer;
+        }
+    }
+}
