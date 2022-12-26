8ef2492a19180428b4c98e46c8674b32b89de463
jwlee <dannis1124@naver.com>
2022-12-26 18:16:33 +0900

    코딩테스트 Lv0 -> 저주의 숫자3 풀이완료

--- src/lv0/day23/CurseNumberTest.java
+++ src/lv0/day23/CurseNumberTest.java
@@ -9,9 +9,9 @@ package lv0.day23;
 public class CurseNumberTest {
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        System.out.println(solution.solution(10));
+        System.out.println(solution.solution(10));
         System.out.println(solution.solution(15));
-//        System.out.println(solution.solution(40));
+        System.out.println(solution.solution(40));
     }
 
     /**
@@ -23,36 +23,72 @@ public class CurseNumberTest {
             int answer = 0;
             boolean isValid = 1 <= n && n <= 100;
             if(isValid){
-                for (int i=1; i <= n; i++) {
-                    answer ++;
-                    if(answer % 3 == 0 ){
-                        answer++;
-                    }
-                    if(answer / 10 > 0) {
-                        answer = convert3Number(answer, 0);
-                    }
-                }
+                answer = process3Number(n);
             }
             return answer;
         }
 
-        public int convert3Number(int n, int cnt) {
+        private int process3Number(int n) {
 
-            int division = n / 10 ;
-            int remainder = n % 10;
+            int result = 0;
+            for (int i=1; i<= n; i++) {
 
-            if(remainder % 3 == 0) {
-                remainder++;
-                if(remainder == 10) {
-                    division++;
+                result++;
+                // case 1) 숫자 3인경우
+                if(isExist3Number(result)) {
+                    result = find3Number(result, 1);
+                    if(result % 3 == 0) {
+                        result++;
+                    }
+                }
+
+                if(result % 3 == 0) {
+                    result++;
+                    if(isExist3Number(result)){
+                        result = find3Number(result, 1);
+                    }
                 }
             }
 
-            if(division == 0) {
-                return (10*cnt) * remainder;
+            return result;
+        }
+
+        private boolean isExist3Number(int n) {
+            boolean result = false;
+            while (true) {
+                int division = n / 10;
+                int remainder = n % 10;
+                if(remainder == 3) {
+                    result = true;
+                    break;
+                }
+
+                if(division == 0) break;
+                n /= 10;
+            }
+            return result;
+        }
+
+        private int find3Number(int n, int position) {
+            if(n/10 == 0) {
+                int a = n % 10;
+                if(a == 3) a++;
+                if(a >= 10) {
+                    return a * (int)Math.pow(10, (position-1)) + find3Number(a/10, position+1);
+                }
+                return a * (int)Math.pow(10, (position-1));
             }
 
-            return (10*cnt)+ remainder  + convert3Number(n/10, cnt+1);
+            int division = n / 10;
+            int remainder = n % 10;
+            if(remainder == 3) {
+                remainder++;
+                if(remainder == 10) {
+                    division++;
+                    remainder = 0;
+                }
+            }
+            return remainder * (int)Math.pow(10, (position-1)) + find3Number(division, position+1);
         }
     }
 }
