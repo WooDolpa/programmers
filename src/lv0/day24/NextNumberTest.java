d5e6de4a3180fc3b6090d6e3ef07543f442f4588
jwlee <dannis1124@naver.com>
2023-01-01 18:01:37 +0900

    코딩테스트 Lv0 -> 다음에 올 숫자 풀이중

---
+++ src/lv0/day24/NextNumberTest.java
@@ -0,0 +1,29 @@
+package lv0.day24;
+
+/**
+ * packageName : lv0.day24
+ * className : NextNumberTest
+ * user : jwlee
+ * date : 2022/12/29
+ */
+public class NextNumberTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 다음에 올 숫자
+     *
+     */
+    public static class Solution {
+        public int solution(int[] common){
+            int answer = 0;
+            boolean isLen = 2 < common.length && common.length < 1000;
+            if(isLen){
+
+            }
+            return answer;
+        }
+    }
+}
